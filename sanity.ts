b07cd948efdd9348d103b15e9d60f7482c88d7d3
rasetsuxabata <rasetsuxabata@gmail.com>
2022-11-20 20:35:12 +0530

    sanity.ts revision

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production", // "production"
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, // "pv8y60vp"
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'mgukrg61', // "pv8y60vp"
     apiVersion: "2022-11-16",
     useCdn: process.env.NODE_ENV === 'production', // server-side is statically generated, the CDN is only necessary beneficial if queries are called on-demand
         
